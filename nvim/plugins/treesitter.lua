64e7009c6f3cc83dd94bc50572457c27d16df9a1
Jason Chambers <jason.d.chambers@gmail.com>
2024-03-28 10:12:46 -0400

    Modularizing lua config by breaking up the init.lua into individual lua files inthe plugins directory. Following https://www.youtube.com/watch?v=4zyZ3sw_ulc

---
+++ nvim/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+return {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+    config = function()
+       local config = require("nvim-treesitter.configs")
+       config.setup({
+         ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+         auto_install = false,
+         sync_install = false,
+         ignore_install = {},
+         modules = {},
+         highlight = { enable = true },
+         indent = { enable = true },
+       })
+    end
+}
