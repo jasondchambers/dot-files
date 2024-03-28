fc1a6a4d7b3ae779fa47a8bc2377995df96c02ad
Jason Chambers <jason.d.chambers@gmail.com>
2024-03-28 10:39:15 -0400

    Copy over entire .config/nvim tree into Git

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,7 @@
+return {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require("gitsigns").setup()
+    end
+}
