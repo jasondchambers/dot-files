a34615d6e7894b31e19efb17865df2973c8a20e6
Jason Chambers <jason.d.chambers@gmail.com>
2024-05-10 16:57:23 -0400

    Refactored into a more module lazy plugin configuration. Cleaned up init.lua a little - still needs cleaning up.

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,7 +1,7 @@
 return {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    config = function()
-      require("gitsigns").setup()
-    end
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require("gitsigns").setup()
+  end
 }
