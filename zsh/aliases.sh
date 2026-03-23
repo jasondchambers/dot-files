0317e1a8b7abeabc8014070d352af3505c66a9a4
Jason Chambers <jason.d.chambers@gmail.com>
2026-03-23 10:45:54 -0400

    Gravitate towards tv

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,4 +1,5 @@
-
 alias ls='eza'   # Use eza instead of ls https://github.com/eza-community/eza
 alias vim='nvim' # Too many years of typing vi and vim
 alias vi='nvim'  # Too many years of typing vi and vim
+alias s='tv ssh'
+alias sf='tv sftp'
