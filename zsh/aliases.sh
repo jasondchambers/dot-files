f49247bd81dca2c380a67a76c875454ffa0c79e3
Jason Chambers <jason.d.chambers@gmail.com>
2026-04-13 16:59:53 -0400

    Use an fzf-powered nvim launcher

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,5 +1,5 @@
 alias ls='eza'   # Use eza instead of ls https://github.com/eza-community/eza
-alias vim='nvim' # Too many years of typing vi and vim
-alias vi='nvim'  # Too many years of typing vi and vim
+alias vim='$HOME/repos/utils/nvimfzf' # Too many years of typing vi and vim
+alias vi='$HOME/repos/utils/nvimfzf'  # Too many years of typing vi and vim
 alias s='tv ssh'
 alias sf='tv sftp'
